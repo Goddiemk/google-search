49432325c2b16dfa8dd8b2bc15f6645cf60d360f
minkhantkk2014@gmail.com <mkkk1331997>
2018-06-11 15:33:38 +0630

    second

--- google_search.py
+++ google_search.py
@@ -1,4 +1,4 @@
-import unittest,re
+import re
 from selenium import webdriver
 from googlesearch import search
 from pyvirtualdisplay import Display
@@ -7,7 +7,7 @@ display = Display(visible=0, size=(1920, 1080)).start()
 driver = webdriver.Chrome()
 query=input("Enter the search word ")
 name = input("Enter your website's name ")
-for sitelist in search(query,tld="com",num=5,stop=1,pause=2):
+for sitelist in search(query,tld="co.in",lang="en",num=5,stop=1,pause=2):
     print(sitelist)
     list=[sitelist]
     for sitelist in list:
