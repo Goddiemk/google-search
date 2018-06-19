4c43361bf958e813ef8dddbe5a702da79877d930
minkhantkk2014@gmail.com <mkkk1331997>
2018-06-19 09:57:25 +0630

    first

--- google_search.py
+++ google_search.py
@@ -1,27 +1,30 @@
-import re,sys
+import re, sys
 from selenium import webdriver
 from googlesearch import search
 from pyvirtualdisplay import Display
+
 
 def main():
 
     display = Display(visible=0, size=(1920, 1080)).start()
     driver = webdriver.Chrome()
 
-    query=sys.argv[1]
-    name =sys.argv[-1]
+    query = sys.argv[1]
+    name = sys.argv[-1]
 
-    for sitelist in search(query,tld="com",lang="en",num=5,start=0,stop=5,pause=2):
+    for sitelist in search(
+            query, tld="com", lang="en", num=5, start=0, stop=5, pause=2):
         print(sitelist)
-        list=[sitelist]
+        list = [sitelist]
         for sitelist in list:
-            final=re.search(name,sitelist)
+            final = re.search(name, sitelist)
             if final:
                 print("Found")
-            else: print("Not Found")
+            else:
+                print("Not Found")
 
     driver.quit()
-    
+
 
 if __name__ == '__main__':
     main()
